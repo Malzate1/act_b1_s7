49a73fa6e65a7ff032191ad3d1ded752706b3e29
Mayra Alzate <nadja.minni@gmail.com>
2025-08-25 22:40:55 -0500

    clase hija producto alimenticio iniciado

--- src/main/java/com/example/ProductoAlimenticio.java
+++ src/main/java/com/example/ProductoAlimenticio.java
@@ -1,5 +1,20 @@
 package com.example;
 
-public class ProductoAlimenticio {
+public class ProductoAlimenticio extends  Producto {
+
+    private int garantiaMeses;
+    private String marca;
+    private double voltaje;
+    private boolean esImportado;
+
+
+    public ProductoAlimenticio(String nombre,double precioBase, String codigo, int cantidadStock, String categoria,int garantiaMeses,String marca,double voltaje,boolean esImportado){
+        super(nombre, precioBase, codigo, cantidadStock, categoria);
+        this.garantiaMeses=garantiaMeses;
+        this.marca=marca;
+        this.voltaje=voltaje;
+        this.esImportado=esImportado;
+    }
+
 
 }
